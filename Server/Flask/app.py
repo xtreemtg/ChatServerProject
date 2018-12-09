46618c2f54082b59d6b4596a41e75173b592e156
xtreemtg <jonah.taurog@mail.yu.edu>
2018-12-09 14:48:12 -0600

    h

--- Server/Flask/app.py
+++ Server/Flask/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request
+
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('home.html')
+@app.route('/result', methods = ['GET', 'POST'])
+def result():
+    if request.method == 'GET':
+        r = request
+        place = request.args.get('first', None)
+        if place:
+            return place
+
+        return "No place information is given"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
